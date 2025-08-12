e50f916e7ba64440b87a55cda59ce735bf5ae76f
Cataldo <fcataldo@ada.middlebury.edu>
2025-08-12 09:58:12 -0400

    parameter heatmaps and algorithm testing

---
+++ scripts/sentate_bills_diff_params.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# SLURM template for serial jobs
+
+# Set SLURM options
+#SBATCH --job-name=slurm_test      # Job name
+#SBATCH --output=slurm_test-%j.out # Output file incorporating job ID
+#SBATCH --partition=standard        # Partition (queue) 
+#SBATCH --array=1-5
+#SBATCH --time=10:00:00             # Time limit hrs:min:sec
+#SBATCH --mem=8G                 # Job memory request 
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=fcataldo@middlebury.edu
+
+
+# Print SLURM environment variables
+echo "Job ID: ${SLURM_JOB_ID}"
+echo "Node: ${SLURMD_NODENAME}" 
+
+# Start of job info
+echo "Starting: "`date +"%D %T"` 
+
+# Your calculations here
+
+echo $PWD
+
+.venv/bin/python senate_bills_algo_test.py -${SLURM_JOB_ID}
+
+
+# End of job info 
+echo "Ending: "`date +"%D %T"`
+
