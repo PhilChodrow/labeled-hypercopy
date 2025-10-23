f0b95d283430cc031394aec9df041758c2f09731
Phil Chodrow <pchodrow@middlebury.edu>
2025-10-23 14:09:32 -0400

    prep merge

--- slurm/run-edge-size-simulator.sh
+++ slurm/run-edge-size-simulator.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 #SBATCH --job-name=toy-edge-size-simulator             
-#SBATCH --array=0-20
-#SBATCH --mem=2gb                              
+#SBATCH --array=0-20                     
 #SBATCH --partition=standard
 #SBATCH --time=24:00:00                        
 #SBATCH --output=throughput/logs/edge-sizes/toy_edge_size_simulator_%a.out
+#SBATCH --mem-per-cpu=20gb
+
 
 # print SLURM environment variables
 echo "Job ID: ${SLURM_JOB_ID}"
